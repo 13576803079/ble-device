cc488d627e6e6096543e4266982dbb717b93d263
yuan-wh <42395227+13576803079@users.noreply.github.com>
2024-03-14 15:45:46 +0800

    Update keyboard.js

--- js/keyboard.js
+++ js/keyboard.js
@@ -16,21 +16,8 @@ function inputNumber(num) {
     
     checkKeyboardStatus();
   }
-    // if (!box.classList.contains('filled')) {
-    //     box.classList.add('filled');
-    //     password += num
-    //     if (currentInputIndex < 4) {
-    //         currentInputIndex++;
-    //     }else{
-    //         isFull = true
-    //     }
-    //     checkKeyboardStatus();
-    // }
 }
 
-// document.querySelector('.key').addEventListener('touchstart', function(e) {  
-//     e.preventDefault();  
-// })
 
 function confirmNumber() {
     if(password.length < 4){
@@ -42,16 +29,12 @@ function deleteNumber() {
   if (currentInputIndex > 1 && document.getElementById('box' + currentInputIndex).textContent === '') {
       currentInputIndex--;
   }
-    // if (currentInputIndex > 1 && !document.getElementById('box' + currentInputIndex).classList.contains('filled')) {
-    //   currentInputIndex--;
-    // }
-    isFull = false
-    password = password.slice(0, currentInputIndex - 1)
-    const boxId = 'box' + currentInputIndex;
-    const box = document.getElementById(boxId);
-    box.textContent = '';
-    // box.classList.remove('filled');
-    checkKeyboardStatus();
+  isFull = false
+  password = password.slice(0, currentInputIndex - 1)
+  const boxId = 'box' + currentInputIndex;
+  const box = document.getElementById(boxId);
+  box.textContent = '';
+  checkKeyboardStatus();
 }
 
 function clearPassword(){
@@ -68,7 +51,6 @@ function clearPassword(){
 
 function checkKeyboardStatus() {
   const keys = document.querySelectorAll('.key');
-//   console.log(keys)
   const keysLength = keys.length
   keys.forEach((key, index) => {
     if (isFull === true && index + 1 < keysLength) {
